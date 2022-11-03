cfc83894318ed57a7ab8ffb9f16e135976826fe6
unknown <python.centaurus@protonmail.com>
2022-11-03 14:29:30 +0100

    Added vulcan h1 element

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>Hello Dawid 🖖</h1>
         <p>Pipelines</p>
         <p>
           Edit <code>src/App.js</code> and save to reload.
